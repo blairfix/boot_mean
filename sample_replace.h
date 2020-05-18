ccc0940e59026890e44a78fae41e7996d131ae26
Blair Fix <blairfix@gmail.com>
2020-05-18 15:48:54 -0400

    fixed comments

--- sample_replace.h
+++ sample_replace.h
@@ -2,26 +2,21 @@
 #ifndef SAMPLE_REPLACE_H
 #define SAMPLE_REPLACE_H
 
-#include <random>
-
 #define ARMA_DONT_USE_WRAPPER
 #define ARMA_NO_DEBUG
 #include <armadillo>
-
-
+#include <random>
 
 // Takes a sample of size "size" out of vector x with replacement.
 // Code is taken from the RcppArmadillo sample.h template:
 
 
-
 arma::vec sample_replace(const arma::vec &x, int size)
 {
 
-    std::random_device rd;  //Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::random_device rd;  
+    std::mt19937 gen(rd()); 
     std::uniform_real_distribution<> dis(0, 1);
-
 
     arma::vec ret(size);
     int nOrig = x.size();
@@ -35,7 +30,5 @@ arma::vec sample_replace(const arma::vec &x, int size)
     return ret;
 
 }
-
-
 
 #endif
